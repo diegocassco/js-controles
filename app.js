e1f0ff2a1106cca309a4af9c84593611936cb3a2
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-08-05 15:32:50 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+    DOMContentLoaded sirve para controlar
+    que primero se cargue TODO el documento HTML, por ultimo
+    javascript
+*/
+document
+.addEventListener("DOMContentLoaded", function(){
+    //esta zona es segura
+    //console.log(document.getElementById("txtNombre").value);
+    //console.log(document.getElementById("txtApellido").value);
+});
+// anadiendo evento clic al mostrar
+const btnMostrar = document.getElementById("btnMostrar")
+const btnMostrar = document.getElementById("btnMostrar")
+.addEventListener("click", function() )
+
+
+function hola(nombre, apellido) {
+    console.log(nombre, apellido);
+    //codigo reutilizable
+}
